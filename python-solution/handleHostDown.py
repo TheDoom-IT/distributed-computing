4fbf37b02e33dc6946b491ec144a660f35c39378
Sunil Ratnayake <sunil.ratnayake12@gmail.com>
2024-06-16 18:58:45 +0200

    optimised election process a little

--- python-solution/handleHostDown.py
+++ python-solution/handleHostDown.py
@@ -6,11 +6,11 @@ from log import log
 
 
 
+
 def handleHostDown(hostId):
 	elections[hostId] = getTimestamp()
 	print("voting host down: ", hostId)
 	print("gathering /election results")
-	electionWon = True
 	for node_id in thisNode.known_nodes:
 		if node_id != hostId:
 			node = thisNode.known_nodes[node_id]
@@ -22,42 +22,38 @@ def handleHostDown(hostId):
 				if(obj['response'] == "OK"):
 					pass
 				else:
-					electionWon = False
+					print("election lost")
+					return
 			except requests.exceptions.RequestException as e:
 				print("exception: ", e)
 				log("error sending election to node: ",node.node_id,"\n", traceback.format_exc())
+	
+	print("election won")
+	for i in range(len(votings)):
+		if votings[i].host_node_id == hostId:
+			votings[i].host_node_id = thisNode.node_id
+	for nodeId in thisNode.known_nodes:
+		if nodeId != hostId:
+			node = thisNode.known_nodes[nodeId]
+			js = {'nodeId':thisNode.node_id,'oldHostId':hostId}
+			url = "http://"+str(node.node_ip)+":"+str(node.node_port)+"/election-results"
+			try:
+				x = requests.post(url, json=js)
+				print("request response: ", x)
+				obj = json.loads(x.content.decode("utf-8"))
+				#TODO write this stuff here
+				votes = obj["votes"]
+				for vote in votes:
+					print("recovered vote: ", vote)
+					votingId = vote[0]
+					voteOption = vote[1]
+					print("vote votingId: ", votingId, " vote option: ", voteOption)
+					if voteOption != None:
+						for i in range(len(votings)):
+							if votings[i].voting_id == votingId:
+								votings[i].castVote(nodeId, voteOption)
 
-
-	if electionWon:
-		print("election won")
-		for i in range(len(votings)):
-			if votings[i].host_node_id == hostId:
-				votings[i].host_node_id = thisNode.node_id
-		for nodeId in thisNode.known_nodes:
-			if nodeId != hostId:
-				node = thisNode.known_nodes[nodeId]
-				js = {'nodeId':thisNode.node_id,'oldHostId':hostId}
-				url = "http://"+str(node.node_ip)+":"+str(node.node_port)+"/election-results"
-				try:
-					x = requests.post(url, json=js)
-					print("request response: ", x)
-					obj = json.loads(x.content.decode("utf-8"))
-					#TODO write this stuff here
-					votes = obj["votes"]
-					for vote in votes:
-						print("recovered vote: ", vote)
-						votingId = vote[0]
-						voteOption = vote[1]
-						print("vote votingId: ", votingId, " vote option: ", voteOption)
-						if voteOption != None:
-							for i in range(len(votings)):
-								if votings[i].voting_id == votingId:
-									votings[i].castVote(nodeId, voteOption)
-
-					
-				except requests.exceptions.RequestException as e:
-					print("exception: ", e)
-					log("error sending election results: ",node.nodeId,"\n", traceback.format_exc())
-
-	else:
-		print("election lost")
+				
+			except requests.exceptions.RequestException as e:
+				print("exception: ", e)
+				log("error sending election results: ",node.nodeId,"\n", traceback.format_exc())
